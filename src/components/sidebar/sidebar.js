df77c84af6669154e2e2ad9e8ff638186bfe871d
Dmytro Izyuk <dizyuk@echoua.com>
2019-03-01 17:57:07 +0200

    iungo-frontend: Campaigns link is removed

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -31,15 +31,15 @@ class Sidebar extends Component {
                         Captive Portals
                     </span>
                 </NavLink>
-                <NavLink to="/campaigns" activeClassName='active'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="#BFC5D2" fillRule="nonzero"
-                              d="M7.5 8C5.6 8 4 9.6 4 11.5S5.6 15 7.5 15h.1l2.6 4.3c.4.7 1.3.9 2.1.5.7-.4.9-1.3.5-2.1L11.1 15H13l7 4V4l-7 4H7.5z"/>
-                    </svg>
-                    <span>
-                        Campaigns
-                    </span>
-                </NavLink>
+                {/*<NavLink to="/campaigns" activeClassName='active'>*/}
+                    {/*<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">*/}
+                        {/*<path fill="#BFC5D2" fillRule="nonzero"*/}
+                              {/*d="M7.5 8C5.6 8 4 9.6 4 11.5S5.6 15 7.5 15h.1l2.6 4.3c.4.7 1.3.9 2.1.5.7-.4.9-1.3.5-2.1L11.1 15H13l7 4V4l-7 4H7.5z"/>*/}
+                    {/*</svg>*/}
+                    {/*<span>*/}
+                        {/*Campaigns*/}
+                    {/*</span>*/}
+                {/*</NavLink>*/}
                 <NavLink to="/reports/people" activeClassName='active'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <g fill="#BFC5D2" fillRule="nonzero">
