07fa8e8df85915ef53473ad81ca7e4260609f2e8
Dmytro Izyuk <dizyuk@echoua.com>
2019-03-27 15:58:43 +0200

    iungo-frontend: #74

--- src/components/captivePortal/optionsSidebar/tabs/contentComponents/loginMethods/loginMethods.js
+++ src/components/captivePortal/optionsSidebar/tabs/contentComponents/loginMethods/loginMethods.js
@@ -77,6 +77,7 @@ class LoginMethods extends Component {
                                             </svg>
                                         </span>
                                     </div>
+                                    <span>Facebook button</span>
                                 </label>
                                 <label htmlFor="google">
                                     <span className="checkBoxPlace">
@@ -92,12 +93,27 @@ class LoginMethods extends Component {
                                             </svg>
                                         </span>
                                     </div>
+                                    <span>Google button</span>
                                 </label>
+                                {/*<label htmlFor="twitter">*/}
+                                    {/*<span className={this.props.style.checkBoxPlace}>*/}
+                                        {/*<input type="checkbox" id="twitter" onChange={this.checkBoxHandler}/>*/}
+                                        {/*<span></span>*/}
+                                    {/*</span>*/}
+                                    {/*<div className={this.props.style.tw}>*/}
+                                        {/*<span>*/}
+                                            {/*<svg xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14">*/}
+                                                {/*<path fill="#FFF" fillRule="nonzero"*/}
+                                                      {/*d="M16 2c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1C9.3.5 7.8 2 7.8 3.8c0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 6.7 1.8 8 3.3 8.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 3.3 15.6 2.7 16 2z"/>*/}
+                                            {/*</svg>*/}
+                                        {/*</span>*/}
+                                    {/*</div>*/}
+                                {/*<span>Twitter button</span>*/}
+                                {/*</label>*/}
                                 <label htmlFor="button">
                                     <span className="checkBoxPlace">
                                         <input ref={this.button} type="checkbox" id="button" onChange={this.checkBoxHandler}/>
                                         <span></span>
-
                                     </span>
                                     <div>
                                         <span>
@@ -106,6 +122,7 @@ class LoginMethods extends Component {
                                             </svg>
                                         </span>
                                     </div>
+                                    <span>Connect button</span>
                                 </label>
                                 {this.state.button && <AgreementButton acceptButton={this.props.acceptButton}/>}
 
