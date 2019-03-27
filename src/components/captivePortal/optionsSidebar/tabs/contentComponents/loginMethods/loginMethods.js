3278ab2236207e23bb5d7796fdcae38d437bb079
Dmytro Izyuk <dizyuk@echoua.com>
2019-03-27 15:28:57 +0200

    iungo-frontend: #88

--- src/components/captivePortal/optionsSidebar/tabs/contentComponents/loginMethods/loginMethods.js
+++ src/components/captivePortal/optionsSidebar/tabs/contentComponents/loginMethods/loginMethods.js
@@ -101,8 +101,8 @@ class LoginMethods extends Component {
                                     </span>
                                     <div>
                                         <span>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="26" viewBox="0 0 24 24">
-                                                <path style={{fill: '#5585ed'}} d="M20 12.194v9.806h-20v-20h18.272l-1.951 2h-14.321v16h16v-5.768l2-2.038zm.904-10.027l-9.404 9.639-4.405-4.176-3.095 3.097 7.5 7.273 12.5-12.737-3.096-3.096z"/>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                                <path style={{fill: '#ffffff'}} d="M20 12.194v9.806h-20v-20h18.272l-1.951 2h-14.321v16h16v-5.768l2-2.038zm.904-10.027l-9.404 9.639-4.405-4.176-3.095 3.097 7.5 7.273 12.5-12.737-3.096-3.096z"/>
                                             </svg>
                                         </span>
                                     </div>
