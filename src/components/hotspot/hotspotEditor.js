00fb6963c21cd667ed10cc35379474d167610d23
jkosach <jkosach@Vasils-MacBook-Pro.local>
2019-08-12 19:06:22 +0300

    form validation

--- src/components/hotspot/hotspotEditor.js
+++ src/components/hotspot/hotspotEditor.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import Notification from "../additional/notification";
 import {createHotspot, getAllPortals, getHotspotByUUID, updateHotspotById} from "../../api/API";
 import CaptivePortalContext from "../../context/project-context";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const ValidationSchema = Yup.object().shape({
+    name: Yup.string()
+        .required('Required')
+});
 
 class HotspotEditor extends Component {
 
@@ -15,14 +22,15 @@ class HotspotEditor extends Component {
         portalsList: '',
         portalUrl: '',
         submitted: false,
-        submittedType: ''
+        submittedType: '',
     };
 
     static contextType = CaptivePortalContext;
 
     portals = React.createRef();
 
-    handleInputChange = (e) => {
+    handleInputChange = (e, handleChange) => {
+        handleChange && handleChange(e);
         const currentState = this.state;
         const name = e.target.getAttribute('name');
         currentState[name] = e.target.value;
@@ -146,6 +154,16 @@ class HotspotEditor extends Component {
         span.innerText = data;
     }
 
+    getFieldErrorText(errors, touched, fieldName) {
+        let error;
+        if (touched[fieldName]) {
+            if (errors && errors[fieldName]) {
+                error = errors[fieldName];
+            }
+        }
+        return Boolean(error) ? <p className={'errorText'}>{error}</p> : null;
+    }
+
     render() {
         const {
             name,
@@ -167,101 +185,122 @@ class HotspotEditor extends Component {
                         }
                     </div>
                     <div className="contentWrapWithTopBorder hotspot">
-                        <div className="hotspotForm">
-                            <label htmlFor={'hotspot-name'}>Name</label>
-                            <div>
-                                <input
-                                    id={'hotspot-name'}
-                                    type="text"
-                                    name="name"
-                                    placeholder={"Hostpot name"}
-                                    onChange={this.handleInputChange}
-                                    value={name}
-                                />
-                            </div>
-                            <label htmlFor={'hotspot-address'}>Address or location</label>
-                            <div>
-                                <input
-                                    id={'hotspot-address'}
-                                    type="text"
-                                    name="address"
-                                    placeholder={"Hotspot location or address"}
-                                    onChange={this.handleInputChange}
-                                    value={address}
-                                />
-                            </div>
-                            <label htmlFor={'hotspot-description'}>Description</label>
-                            <div className={'withTextarea'}>
-                                <textarea
-                                    id={'hotspot-description'}
-                                    name="description"
-                                    placeholder={"Description"}
-                                    onChange={this.handleInputChange}
-                                    value={description}
-                                >
+                        <Formik ref={el => this._form = el}
+                            initialValues={{ name, address, description }}
+                            validationSchema={ValidationSchema}
+                            validateOnChange={true}
+                            render={({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                submitForm,
+                                isValid
+                            }) => (
+                                <div className="hotspotForm">
+                                    <label htmlFor={'hotspot-name'}>Name</label>
+                                    <div className={Boolean(this.getFieldErrorText(errors, touched, 'name')) ? 'errorField' : ''}>
+                                        <input
+                                            id={'hotspot-name'}
+                                            type="text"
+                                            name="name"
+                                            placeholder={"Hostpot name"}
+                                            onChange={(e) => this.handleInputChange(e, handleChange)}
+                                            onBlur={(e) => this.handleInputChange(e, handleChange)}
+                                            value={values.name}
+                                        />
+                                        {this.getFieldErrorText(errors, touched, 'name')}
+                                    </div>
+                                    <label htmlFor={'hotspot-address'}>Address or location</label>
+                                    <div className={Boolean(this.getFieldErrorText(errors, touched, 'address')) ? 'errorField' : ''}>
+                                        <input
+                                            id={'hotspot-address'}
+                                            type="text"
+                                            name="address"
+                                            placeholder={"Hotspot location or address"}
+                                            onChange={(e) => this.handleInputChange(e, handleChange)}
+                                            onBlur={(e) => this.handleInputChange(e, handleChange)}
+                                            value={address}
+                                        />
+                                        {this.getFieldErrorText(errors, touched, 'address')}
+                                    </div>
+                                    <label htmlFor={'hotspot-description'}>Description</label>
+                                    <div className={'withTextarea ' + (Boolean(this.getFieldErrorText(errors, touched, 'address')) ? 'errorField' : '')}>
+                                        <textarea
+                                            id={'hotspot-description'}
+                                            name="description"
+                                            placeholder={"Description"}
+                                            onChange={(e) => this.handleInputChange(e, handleChange)}
+                                            onBlur={(e) => this.handleInputChange(e, handleChange)}
+                                            value={description}
+                                        >
 
-                                </textarea>
-                            </div>
-                            <label htmlFor={'select-captive-portal'}>Select Captive Portal</label>
-                            <div className={'profileDetails'}>
-                                <select name="portals"
-                                        id={'select-captive-portal'}
-                                        ref={this.portals}
-                                        onChange={this.selectHandler}>
-                                    <option value="">Choose portal</option>
+                                        </textarea>
+                                        {this.getFieldErrorText(errors, touched, 'description')}
+                                    </div>
+                                    <label htmlFor={'select-captive-portal'}>Select Captive Portal</label>
+                                    <div className={'profileDetails'}>
+                                        <select name="portals"
+                                                id={'select-captive-portal'}
+                                                ref={this.portals}
+                                                onChange={this.selectHandler}>
+                                            <option value="">Choose portal</option>
+                                            {
+                                                portalsList !== '' &&
+                                                portalsList.map((item, i) => {
+                                                    console.log(item);
+                                                    return <option key={i} dataid={item.id} selected={captivePortalID === item.id}
+                                                                portalurl={item.externalUrl}>{item.name}</option>
+                                                })
+                                            }
+                                        </select>
+                                        <p className="select">
+                                            <span>Choose portal</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                                                <path fill="#ffffff" fillRule="nonzero"
+                                                    d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
+                                            </svg>
+                                        </p>
+                                    </div>
+                                    <button onClick={isValid ? this.handleCorrect : submitForm}>Save</button>
+
+
                                     {
-                                        portalsList !== '' &&
-                                        portalsList.map((item, i) => {
-                                            console.log(item);
-                                            return <option key={i} dataid={item.id} selected={captivePortalID === item.id}
-                                                           portalurl={item.externalUrl}>{item.name}</option>
-                                        })
-                                    }
-                                </select>
-                                <p className="select">
-                                    <span>Choose portal</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
-                                        <path fill="#ffffff" fillRule="nonzero"
-                                              d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
-                                    </svg>
-                                </p>
-                            </div>
-                            <button onClick={this.handleCorrect}>Save</button>
-
-
-                            {
-                                (localStorage.getItem('HSurl') !== 'null' && localStorage.getItem('HSurl')) &&
-                                <p className="link">
-                                    <span>
-                                        Copy and paste this URL to your device settings<br />
-                                        <br />
-                                        {localStorage.getItem('HSurl')}
-                                    </span>
-                                    <span onClick={this.copyToClipboard}>
-                                        <svg version="1.1" id="Capa_1"
-                                             x="0px" y="0px" viewBox="0 0 488.3 488.3"
-                                             style={{enableBackground: 'new 0 0 488.3 488.3'}}
-                                             width="20px" height="20px" className="">
-                                            <g>
-                                                <g>
+                                        (localStorage.getItem('HSurl') !== 'null' && localStorage.getItem('HSurl')) &&
+                                        <p className="link">
+                                            <span>
+                                                Copy and paste this URL to your device settings<br />
+                                                <br />
+                                                {localStorage.getItem('HSurl')}
+                                            </span>
+                                            <span onClick={this.copyToClipboard}>
+                                                <svg version="1.1" id="Capa_1"
+                                                    x="0px" y="0px" viewBox="0 0 488.3 488.3"
+                                                    style={{enableBackground: 'new 0 0 488.3 488.3'}}
+                                                    width="20px" height="20px" className="">
                                                     <g>
-                                                        <path
-                                                            d="M314.25,85.4h-227c-21.3,0-38.6,17.3-38.6,38.6v325.7c0,21.3,17.3,38.6,38.6,38.6h227c21.3,0,38.6-17.3,38.6-38.6V124    C352.75,102.7,335.45,85.4,314.25,85.4z M325.75,449.6c0,6.4-5.2,11.6-11.6,11.6h-227c-6.4,0-11.6-5.2-11.6-11.6V124    c0-6.4,5.2-11.6,11.6-11.6h227c6.4,0,11.6,5.2,11.6,11.6V449.6z"
-                                                            className="active-path" fill="#5585ED"/>
-                                                        <path
-                                                            d="M401.05,0h-227c-21.3,0-38.6,17.3-38.6,38.6c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5c0-6.4,5.2-11.6,11.6-11.6h227    c6.4,0,11.6,5.2,11.6,11.6v325.7c0,6.4-5.2,11.6-11.6,11.6c-7.5,0-13.5,6-13.5,13.5s6,13.5,13.5,13.5c21.3,0,38.6-17.3,38.6-38.6    V38.6C439.65,17.3,422.35,0,401.05,0z"
-                                                            className="active-path" fill="#5585ED"/>
+                                                        <g>
+                                                            <g>
+                                                                <path
+                                                                    d="M314.25,85.4h-227c-21.3,0-38.6,17.3-38.6,38.6v325.7c0,21.3,17.3,38.6,38.6,38.6h227c21.3,0,38.6-17.3,38.6-38.6V124    C352.75,102.7,335.45,85.4,314.25,85.4z M325.75,449.6c0,6.4-5.2,11.6-11.6,11.6h-227c-6.4,0-11.6-5.2-11.6-11.6V124    c0-6.4,5.2-11.6,11.6-11.6h227c6.4,0,11.6,5.2,11.6,11.6V449.6z"
+                                                                    className="active-path" fill="#5585ED"/>
+                                                                <path
+                                                                    d="M401.05,0h-227c-21.3,0-38.6,17.3-38.6,38.6c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5c0-6.4,5.2-11.6,11.6-11.6h227    c6.4,0,11.6,5.2,11.6,11.6v325.7c0,6.4-5.2,11.6-11.6,11.6c-7.5,0-13.5,6-13.5,13.5s6,13.5,13.5,13.5c21.3,0,38.6-17.3,38.6-38.6    V38.6C439.65,17.3,422.35,0,401.05,0z"
+                                                                    className="active-path" fill="#5585ED"/>
+                                                            </g>
+                                                        </g>
                                                     </g>
-                                                </g>
-                                            </g>
-                                        </svg>
+                                                </svg>
 
-                                    </span>
-                                </p>
+                                            </span>
+                                        </p>
 
-                            }
+                                    }
 
-                        </div>
+                                </div>
+
+                            )}
+                        />  
                         <div className={'portalPreviewWrap'}>
                             <div className="portalPreview">
                                 {
