9708be290e02f9e5094fd7c387f9b878d7538ad7
dizyuk <dizyuk@echoua.com>
2019-08-13 10:50:35 +0300

    iungo-commons: #40

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import './index.less';
 import all from './reducers/reducers';
 import {Provider} from 'react-redux';
 
-import {createStore, applyMiddleware} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 
 import App from './App';
 
+import TagManager from 'react-gtm-module'
+import registerServiceWorker from './registerServiceWorker';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-PP297WL'
+};
+
+TagManager.initialize(tagManagerArgs);
+
 
 const promiseMiddleware = require('redux-promise').default;
-
-import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
   all,
