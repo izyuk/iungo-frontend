0039dcac84d65a833de711fc98e4993bfe997e2b
jkosach <jkosach@Vasils-MacBook-Pro.local>
2019-08-19 15:27:39 +0300

    fix Do not set portal drop down to default when user swicth between tabs on reports page

--- src/components/reports/people.js
+++ src/components/reports/people.js
@@ -1,78 +1,35 @@
 import React, {Component} from 'react';
 import Notification from "../additional/notification";
-import {exportHotspotUsersCSV, getHotspots, getHotspotUsers} from "../../api/API";
-import {connect} from "react-redux";
+import {exportHotspotUsersCSV} from "../../api/API";
 import {dateISO} from '../../modules/dateISO';
 
 class People extends Component {
     state = {
-        list: '',
-        usersList: '',
-        uuid: '',
         UUIDChecker: false,
         notificationText: 'Nothing to export'
     };
     token = localStorage.getItem('token');
 
     shouldComponentUpdate(nextProps, nextState) {
-        if (this.state.list !== nextProps.list) return true;
-        else if (this.state.usersList !== nextProps.usersList) return true;
-        else if (this.state.uuid !== nextProps.uuid) return true;
-        else if (this.state.UUIDChecker !== nextProps.UUIDChecker) return true;
-        else if (this.state.notificationText !== nextProps.notificationText) return true;
-        else return false
+        return (this.state.UUIDChecker !== nextProps.UUIDChecker) ||
+                (this.state.notificationText !== nextProps.notificationText);
     }
-
-    toggleCPSelectorActive = (e) => {
-        e.currentTarget.classList.toggle("active");
-    };
-
-
-    getHotspotsMethodHandler = async (str) => {
-        const query = getHotspots(str);
-        await query.then(res => {
-            const {data} = res;
-            this.setState({
-                list: data
-            })
-        });
-    };
-
-    componentDidMount() {
-        this.getHotspotsMethodHandler(this.token);
-    }
-
-    setCPId = async (e, uuid, id) => {
-        this.setState({
-            uuid: uuid
-        });
-        document.getElementsByClassName('selectedPortal')[0].childNodes[0].innerText = e.currentTarget.innerText;
-        this.setState({captivePortalID: e.currentTarget.getAttribute('dataid') ? e.currentTarget.getAttribute('dataid') : ''});
-        const query = getHotspotUsers(this.token, uuid);
-        await query.then(res => {
-            const {data} = res;
-            this.setState({
-                usersList: data
-            })
-        });
-    };
 
     exportCSV = async () => {
-        if (this.state.uuid) {
+        const { uuid } = this.props;
+        if (uuid) {
             this.setState({UUIDChecker: false});
-            const query = exportHotspotUsersCSV(this.token, this.state.uuid);
+            const query = exportHotspotUsersCSV(this.token, uuid);
             await query.then(res => {
                 let {data} = res;
                 let filename, link;
                 if (data == '' || data == null) {
                     this.setState({
-                        notificationText: 'Nothing to export'
-                    });
-                    this.setState({
+                        notificationText: 'Nothing to export',
                         UUIDChecker: true
                     });
                     setTimeout(() => {
-                        this.setState({copied: false});
+                        this.setState({UUIDChecker: false});
                     }, 2000)
                 } else {
                     this.setState({UUIDChecker: false});
@@ -89,56 +46,23 @@ class People extends Component {
                     link.setAttribute('href', data);
                     link.setAttribute('download', filename);
                 }
-                link.click();
+                link && link.click();
             });
         } else {
             this.setState({
-                notificationText: 'Choose hotspot form list'
-            });
-            this.setState({
+                notificationText: 'Choose hotspot form list',
                 UUIDChecker: true
             });
             setTimeout(() => {
-                this.setState({copied: false});
+                this.setState({UUIDChecker: false});
             }, 2000)
         }
 
     };
 
     render() {
+        const { usersList } = this.props;
         return (
-            <div className={'container'}>
-                <div className="setInfoLine">
-                    <div className={"hsSelectorHead"}>
-                        <div className="hsSelector" onClick={this.toggleCPSelectorActive}>
-                            <p className={'selectedPortal'}>
-                                <span>
-                                    Select Hotspot
-                                </span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="#FFF" fillRule="nonzero"
-                                          d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
-                                </svg>
-
-                            </p>
-                            <div className="list">
-                                {this.state.list !== '' && this.state.list.map((item, i) => {
-                                    return <p onClick={(e) => this.setCPId(e, item.uuid, item.id)} key={i}
-                                              dataid={item.id}>{item.name}</p>
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="exportCSV">
-                        <button type='button' onClick={this.exportCSV}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path fill="#BFC5D2" fillRule="nonzero"
-                                      d="M17 11.1V11c0-2.8-2.2-5-5-5-2.5 0-4.6 1.8-4.9 4.3-1.8.6-3.1 2.2-3.1 4.2C4 17 6 19 8.5 19H16c2.2 0 4-1.8 4-4 0-1.9-1.3-3.4-3-3.9zM11 13v-3h2v3h3l-4 4-4-4h3z"/>
-                            </svg>
-                            <span>Export to CSV</span>
-                        </button>
-                    </div>
-                </div>
                 <table className={"peopleTable"} rules="rows">
                     <thead>
                     <tr>
@@ -151,7 +75,7 @@ class People extends Component {
                     </thead>
                     <tbody>
                     {
-                        this.state.usersList !== '' && this.state.usersList.map((item, i) => {
+                        (usersList && usersList.length) ? usersList.map((item, i) => {
                             return (
                                 <tr key={i}>
                                     {/*<td><input type="checkbox"/></td>*/}
@@ -161,20 +85,16 @@ class People extends Component {
                                     <td>{item.totalVisit}</td>
                                 </tr>
                             )
-                        })
+                        }) : <tr>
+                            <td colSpan='4'>Nothing to show</td>
+                        </tr>
                     }
                     </tbody>
                     {this.state.UUIDChecker && <Notification type={'info'} text={this.state.notificationText}/>}
                 </table>
-            </div>
         )
     }
 }
 
 // export default People;
-export default connect(
-    state => ({
-        token: state.token
-    }),
-    dispatch => ({})
-)(People)
+export default People;
