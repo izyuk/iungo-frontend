cf7b15eed7b7be0c2e85e037558a7daa9360d867
jkosach <jkosach@Vasils-MacBook-Pro.local>
2019-08-23 14:40:31 +0300

    fix page crash when open background settings in Style tab accordion

--- src/components/captivePortal/optionsSidebar/tabs/styleTab.js
+++ src/components/captivePortal/optionsSidebar/tabs/styleTab.js
@@ -10,81 +10,51 @@ class StyleTab extends Component {
     constructor(props){
         super(props);
         this.state={
-            event: '',
-            visible: 'Background',
+            activeTab: 'background'
         };
-        this.Background = React.createRef();
     }
-
-    componentDidMount(){
-        this.setState({
-            event: this.Background.current
-        });
-    }
-
-    componentDidUpdate(){
-        this.state.event.closest(".head").classList.add("active");
-        this.state.event.closest(".head").nextSibling.classList.add("active");
-    }
-
-    dropDownHandler = (e) => {
-
-        this.setState({
-            event: e.currentTarget
-        });
-        for (let i = 0; i < document.querySelectorAll(".head").length; i++) {
-            document.querySelectorAll(".head")[i].classList.remove("active");
-        }
-        this.setState({
-            visible: e.currentTarget.childNodes[0].innerHTML
-        });
-    };
 
     render(){
+        const { activeTab } = this.state;
         const type = this.context.previewDeviceType;
+        console.log('!#! activeTab', activeTab === 'logo');
         return(
             <div className="dropdown">
                 <div className="wrap">
-                    <div
-                        className="head"
+                    <div className={'head ' + ((activeTab === 'background') ? 'active' : '')}
                         data-cy="backgroundDropDown"
-                        onClick={this.dropDownHandler}
-                        ref={this.Background}>
+                        onClick={() => this.setState({ activeTab: 'background' })}
+                    >
                         <span>{(type === 'desktop') ? 'Desktop' : 'Mobile'} background</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path fill="#BFC5D2" fillRule="nonzero" d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
                         </svg>
                     </div>
-                    {this.state.visible === 'Background' ?
-                        <Background type="background"/> :
-                        false}
+                    {(activeTab === 'background') ? <Background type="background"/> : false}
                 </div>
                 <div className="wrap">
-                    <div className="head"
-                         data-cy="logoDropDown"
-                         onClick={this.dropDownHandler}>
+                    <div className={'head ' + ((activeTab === 'logo') ? 'active' : '')}
+                        data-cy="logoDropDown"
+                        onClick={() => this.setState({ activeTab: 'logo' })}
+                    >
                         <span>Logo</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path fill="#BFC5D2" fillRule="nonzero" d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
                         </svg>
                     </div>
-                    {this.state.visible === 'Logo' ?
-                        <Logo type="logo"/>:
-                        false}
+                    {(activeTab === 'logo') ? <Logo type="logo"/> : false}
                 </div>
                 <div className="wrap">
-                    <div
-                        className="head"
+                    <div className={'head ' + ((activeTab === 'background') ? 'active' : '')}
                         data-cy="containerDropDown"
-                        onClick={this.dropDownHandler}>
+                        onClick={() => this.setState({ activeTab: 'container' })}
+                    >
                         <span>Container</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path fill="#BFC5D2" fillRule="nonzero" d="M12 15.6l-4.7-4.7 1.4-1.5 3.3 3.3 3.3-3.3 1.4 1.5z"/>
                         </svg>
                     </div>
-                    {this.state.visible === 'Container' ?
-                        <Container /> :
-                        false}
+                    {(activeTab === 'container') ? <Container /> : false}
                 </div>
             </div>
         )
